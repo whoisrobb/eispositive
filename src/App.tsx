392343494b362dc9192a45e061d46bba5fe73baa
Muchiri <developedbyrobbie@gmail.com>
2024-07-30 23:08:06 +0300

    feat: add smooth scroll with lenis

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,21 @@ import ShareYourStory from "./app/share-your-story/share-your-story"
 import Shop from "./app/shop/shop"
 import Testimonials from "./app/testimonials/testimonials"
 import NavTest from "./components/ideas/nav"
+import { useEffect } from "react"
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
