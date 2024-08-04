97a8b68eed7bbbe0beb799adc84621dbb8601c3b
Muchiri <developedbyrobbie@gmail.com>
2024-08-04 19:19:29 +0300

    fix: fix magnetic button hover

--- src/app/home/_components/video-component.tsx
+++ src/app/home/_components/video-component.tsx
@@ -1,6 +1,6 @@
 import Magnetic from "@/components/elements/magnetic";
 import { Button } from "@/components/ui/button";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from 'react';
 
 type VideoComponentProps = {
   source: string;
@@ -8,43 +8,50 @@ type VideoComponentProps = {
 
 const VideoComponent = ({ source }: VideoComponentProps) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     if (videoRef.current) {
-        if (isPlaying) {
-            videoRef.current.play();
-        } else {
-            videoRef.current.pause();
-            videoRef.current.currentTime = 0;
-        }
+      if (isPlaying) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+        videoRef.current.currentTime = 0;
+      }
     }
   }, [isPlaying]);
 
   return (
     <div
-      className="h-36 w-64 overflow-hidden rounded relative group"
-      onMouseEnter={() => setIsPlaying(true)}
-      onMouseLeave={() => setIsPlaying(false)}
+      className="h-36 w-64 overflow-hidden rounded relative border"
+      onMouseEnter={() => {
+        setIsPlaying(true);
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsPlaying(false);
+        setIsHovered(false);
+      }}
     >
-        <video
-            ref={videoRef}
-            src={`https://utfs.io/f/${source}`}
-            className="object-cover h-[100%] w-[100%]"
-            loop
-            muted
-        />
-
-        <div
-            className="absolute scale-0 top-[2.5rem] left-[6rem] group-hover:scale-100 transition-transform duration-300"
-        >
+      <video
+        ref={videoRef}
+        src={`https://utfs.io/f/${source}`}
+        className="object-cover h-[100%] w-[100%]"
+        loop
+        muted
+      />
+      <div
+        className={`absolute h-16 aspect-square rounded-full top-[2.5rem] left-[6rem] transition-transform duration-300 ${
+          isHovered ? 'scale-100' : 'scale-0'
+        }`}
+      >
         <Magnetic>
-            <Button className="h-16 aspect-square rounded-full  flex items-center justify-center">
-                Play
-            </Button>
+          <Button className="h-16 aspect-square rounded-full  flex items-center justify-center">
+            Play
+          </Button>
         </Magnetic>
       </div>
-
     </div>
   );
 };
