4529287b3b9cc3c66084237581a7b66a1f9d913b
Muchiri <developedbyrobbie@gmail.com>
2024-08-06 00:50:09 +0300

    remove: remove code for redundant pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Layout from "./app/layout"
 import About from "./app/about/about"
 import Community from "./app/community/community"
 import EplusTv from "./app/eplustv/eplustv"
-import ShareYourStory from "./app/share-your-story/share-your-story"
-import Shop from "./app/shop/shop"
 import Testimonials from "./app/testimonials/testimonials"
 import NavTest from "./components/ideas/nav"
 import { useEffect, useState } from "react"
@@ -46,8 +44,6 @@ function App() {
             <Route path="about" element={<About />} />
             <Route path="community" element={<Community />} />
             <Route path="eplustv" element={<EplusTv />} />
-            <Route path="share-your-story" element={<ShareYourStory />} />
-            <Route path="shop" element={<Shop />} />
             <Route path="testimonials" element={<Testimonials />} />
             <Route path="nav" element={<NavTest />} />
           </Route>
